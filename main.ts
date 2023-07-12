f7cee57abcb5f28cc366f0636412b4c47d4aac7d
Christoph Daxerer <christoph.daxerer@students.fh-hagenberg.at>
2023-07-12 17:41:52 +0200

    Add more TODOs

--- main.ts
+++ main.ts
@@ -83,23 +83,29 @@ export default class Quill extends Plugin {
 		});
 
 		// Context settings
-		// TODO add command to set a specific note as context for prompt
-		// TODO add special character sequence to enable comments in context files
-		// TODO add setting to set context length for text before cursor
-		// TODO add setting to set context length to sentence or paragraph or section (section is till the last heading)
-		// TODO add setting to see context files
-		// TODO add setting to set and remove context files
-		// TODO add setting to set price limit based on estimated price of prompts
-		// TODO add command to let gpt compress context files
+			// TODO add command to set a specific note as context for prompt
+			// TODO add special character sequence to enable comments in context files
+			// TODO add setting to set context length for text before cursor
+			// TODO add setting to set context length to sentence or paragraph or section (section is till the last heading)
+			// TODO add setting to see context files
+			// TODO add setting to set and remove context files
+			// TODO add setting to set price limit based on estimated price of prompts
+			// TODO add adjectives list to set task for rewrite command (reduce, rephrase, expand, detail, etc.)
+		// Commands
+			// TODO add command to let gpt compress context files
+			// TODO add command to let gpt rewrite selected text without context files (shallow rewrite)
+			// TODO add command to let gpt rewrite selected text with context files (deep rewrite)
+				// the rewrite command task (reduce, rephrase, expand, detail, etc.) are determined by the adjectives list
+			// TODO add commands to context menu (right click on selected text)
 		// Model settings
-		// TODO add settings for api key (hidden via password field)
-		// TODO add settings for model parameters
-		// Side Tab view
-		// TODO add show and edit for important settings that change frequently (context files, context length, model parameters)
-			// keep context files in a list with enabled/disabled toggle and remove button for fast configuration
-		// TODO add token estimate for context to side tab view (based on OpenAI estimate function)
-		// TODO add price estimate for context to side tab view (based on token estimate and configurable api price)
-		// TODO add request statistics to side tab view (how many requests sent, how many tokens used)
+			// TODO add settings for api key (hidden via password field)
+			// TODO add settings for model parameters
+		// Side Tab view (right side of obsidian)
+			// TODO add show and edit for important settings that change frequently (context files, context length, model parameters)
+				// keep context files in a list with enabled/disabled toggle and remove button for fast configuration
+			// TODO add token estimate for context to side tab view (based on OpenAI estimate function)
+			// TODO add price estimate for context to side tab view (based on token estimate and configurable api price)
+			// TODO add request statistics to side tab view (how many requests sent, how many tokens used)
 
 		this.registerEditorExtension(ViewPlugin.fromClass(RenderPlugin, renderPluginSpec));
 		this.registerEditorExtension(suggestionField);
