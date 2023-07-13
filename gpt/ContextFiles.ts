eb65f9f46b8de9159dce6eaec93dae44d43ba401
Christoph Daxerer <christoph.daxerer@students.fh-hagenberg.at>
2023-07-13 16:36:27 +0200

    Rewrite file read function, apply foramtting

--- gpt/ContextFiles.ts
+++ gpt/ContextFiles.ts
@@ -1,51 +1,49 @@
 export class ContextFile {
-    private path: string;
-    private enabled: boolean;
+  private path: string;
+  private enabled: boolean;
 
-    constructor(path: string, enabled: boolean) {
-        this.path = path;
-        this.enabled = enabled;
-    }
+  constructor(path: string, enabled: boolean) {
+    this.path = path;
+    this.enabled = enabled;
+  }
 
-    getPath(): string {
-        return this.path;
-    }
+  getPath(): string {
+    return this.path;
+  }
 
-    setPath(path: string) {
-        this.path = path;
-    }
+  setPath(path: string) {
+    this.path = path;
+  }
 
-    isEnabled(): boolean {
-        return this.enabled;
-    }
+  isEnabled(): boolean {
+    return this.enabled;
+  }
 
-    setEnabled(enabled: boolean) {
-        this.enabled = enabled;
-    }
+  setEnabled(enabled: boolean) {
+    this.enabled = enabled;
+  }
 }
 
 export class ContextFiles {
-    private contextFiles: ContextFile[] = [];
+  private contextFiles: ContextFile[] = [];
 
-    add(path: string, enabled: boolean = true) {
-        this.contextFiles.push(new ContextFile(path, enabled));
+  add(path: string, enabled: boolean = true) {
+    this.contextFiles.push(new ContextFile(path, enabled));
+  }
+
+  getAll(): ContextFile[] {
+    return this.contextFiles;
+  }
+
+  remove(path: string) {
+    for (let i = 0; i < this.contextFiles.length; i++) {
+      if (this.contextFiles[i].getPath() === path) {
+        this.contextFiles.splice(i, 1);
+      }
     }
+  }
 
-    getAll(): ContextFile[] {
-        return this.contextFiles;
-    }
-
-    remove(path: string) {
-        for (let i = 0; i < this.contextFiles.length; i++) {
-            if (this.contextFiles[i].getPath() === path) {
-                this.contextFiles.splice(i, 1);
-            }
-        }
-    }
-
-    clear() {
-        this.contextFiles = [];
-    }
-
-    
+  clear() {
+    this.contextFiles = [];
+  }
 }
--- main.ts
+++ main.ts
@@ -1,140 +1,176 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
-import { ViewPlugin, PluginValue, ViewUpdate, EditorView } from '@codemirror/view';
-import { RenderPlugin, renderPluginSpec } from 'inline_suggestions/RenderPlugin';
-import { suggestionField, setSuggestion, setSuggestionText } from 'inline_suggestions/InlineSuggestionStateField';
-import { QuillMenuView, VIEW_TYPE_QUILL_MENU } from 'side_view/QuillView';
-import { Fetcher, MockFetcher } from 'gpt/Fetcher';
-import { ContextFiles, ContextFile } from 'gpt/ContextFiles';
-import { QuillSettingTab } from 'settings/SettingTab';
-import { QuillSettings, DEFAULT_SETTINGS } from 'settings/Settings';
-
-
+import {
+  App,
+  Editor,
+  MarkdownView,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  TFile,
+} from "obsidian";
+import {
+  ViewPlugin,
+  PluginValue,
+  ViewUpdate,
+  EditorView,
+} from "@codemirror/view";
+import {
+  RenderPlugin,
+  renderPluginSpec,
+} from "inline_suggestions/RenderPlugin";
+import {
+  suggestionField,
+  setSuggestion,
+  setSuggestionText,
+} from "inline_suggestions/InlineSuggestionStateField";
+import { QuillMenuView, VIEW_TYPE_QUILL_MENU } from "side_view/QuillView";
+import { Fetcher, MockFetcher } from "gpt/Fetcher";
+import { ContextFiles, ContextFile } from "gpt/ContextFiles";
+import { QuillSettingTab } from "settings/SettingTab";
+import { QuillSettings, DEFAULT_SETTINGS } from "settings/Settings";
 
 export default class Quill extends Plugin {
-	settings: QuillSettings;
-	fetcher: Fetcher = new MockFetcher();
-	contextFiles: ContextFiles = new ContextFiles;
+  settings: QuillSettings;
+  fetcher: Fetcher = new MockFetcher();
+  contextFiles: ContextFiles = new ContextFiles();
 
-	
-	private async getTextForFile(path: string): Promise<string> {
-		let text: string = '';
-		const { vault } = this.app;
-	  
-		const file = app.vault.getAbstractFileByPath(path);
-		if (file !== null && file instanceof TFile) {
-		  text = await new Promise<string>((resolve) => {
-			vault.read(file).then((result: string) => {
-			  resolve(result);
-			});
-		  });
-		}
-	  
-		return text;
-	}
+  private async getTextForFile(path: string) {
+    let text: string = "";
+    const { vault } = this.app;
+    const file = app.vault.getAbstractFileByPath(path);
 
-	async onload() {
-		await this.loadSettings();
+    if (file !== null && file instanceof TFile) {
+      text = await vault.cachedRead(file);
+    }
+    return text;
+  }
 
-		this.registerView(VIEW_TYPE_QUILL_MENU, (leaf) => new QuillMenuView(leaf));
+  async onload() {
+    await this.loadSettings();
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new QuillSettingTab(this.app, this));
-		
-		this.addRibbonIcon("feather", "Activate Quill Menu", () => {
+    this.registerView(
+      VIEW_TYPE_QUILL_MENU,
+      (leaf) => new QuillMenuView(leaf)
+    );
+
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new QuillSettingTab(this.app, this));
+
+    this.addRibbonIcon("feather", "Activate Quill Menu", () => {
       this.activateQuillMenuView();
     });
 
-		this.addCommand({
-			id: 'accept-word-from-suggestion',
-			name: 'Accept word from suggestion',
-			hotkeys: [{ modifiers: ["Mod"], key: "ArrowRight" }],
-			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
-				// @ts-expect-error, not typed
-				const editorView = view.editor.cm as EditorView;
-				const suggestionText = editorView.state.field(suggestionField).suggestionText;
+    this.addCommand({
+      id: "accept-word-from-suggestion",
+      name: "Accept word from suggestion",
+      hotkeys: [{ modifiers: ["Mod"], key: "ArrowRight" }],
+      editorCheckCallback: (
+        checking: boolean,
+        editor: Editor,
+        view: MarkdownView
+      ) => {
+        // @ts-expect-error, not typed
+        const editorView = view.editor.cm as EditorView;
+        const suggestionText =
+          editorView.state.field(suggestionField).suggestionText;
 
-				if (suggestionText.length > 0) {
-					if (!checking) {
-						// insert first word from suggestion
-						const firstWord = suggestionText.split(' ')[0] + ' ';
-						editor.replaceSelection(firstWord);
-						// update suggestion state
-						// @ts-expect-error, not typed
-						const editorView = view.editor.cm as EditorView;
-						setSuggestionText(editorView, suggestionText.slice(firstWord.length));
-					}
-					return true
-				}
-				return false;
-			},
-		});
+        if (suggestionText.length > 0) {
+          if (!checking) {
+            // insert first word from suggestion
+            const firstWord = suggestionText.split(" ")[0] + " ";
+            editor.replaceSelection(firstWord);
+            // update suggestion state
+            // @ts-expect-error, not typed
+            const editorView = view.editor.cm as EditorView;
+            setSuggestionText(
+              editorView,
+              suggestionText.slice(firstWord.length)
+            );
+          }
+          return true;
+        }
+        return false;
+      },
+    });
 
-		this.addCommand({
-			id: 'accept-suggestion',
-			name: 'Accept suggestion',
-			hotkeys: [{ modifiers: [], key: "Tab" }],
-			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
-				// @ts-expect-error, not typed
-				const editorView = view.editor.cm as EditorView;
-				const suggestionText = editorView.state.field(suggestionField).suggestionText;
+    this.addCommand({
+      id: "accept-suggestion",
+      name: "Accept suggestion",
+      hotkeys: [{ modifiers: [], key: "Tab" }],
+      editorCheckCallback: (
+        checking: boolean,
+        editor: Editor,
+        view: MarkdownView
+      ) => {
+        // @ts-expect-error, not typed
+        const editorView = view.editor.cm as EditorView;
+        const suggestionText =
+          editorView.state.field(suggestionField).suggestionText;
 
-				if (suggestionText.length > 0) {
-					if (!checking) {
-						// insert whole suggestion
-						editor.replaceSelection(suggestionText);
-						// update suggestion state to empty
-						setSuggestionText(editorView, '');
-					}
-					return true
-				}
-				return false;
-			},
-		});
+        if (suggestionText.length > 0) {
+          if (!checking) {
+            // insert whole suggestion
+            editor.replaceSelection(suggestionText);
+            // update suggestion state to empty
+            setSuggestionText(editorView, "");
+          }
+          return true;
+        }
+        return false;
+      },
+    });
 
-		this.addCommand({
-			id: 'generate-suggestion',
-			name: 'Generate a suggestion',
-			hotkeys: [{ modifiers: ["Mod"], key: "e" }],
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				// @ts-expect-error, not typed
-				const editorView = view.editor.cm as EditorView;
-				const prompt = 'This is a prompt'; // TODO get context for prompt
-				this.fetcher.fetch(prompt).then((suggestion) => setSuggestionText(editorView, suggestion));
-			},
-		});
+    this.addCommand({
+      id: "generate-suggestion",
+      name: "Generate a suggestion",
+      hotkeys: [{ modifiers: ["Mod"], key: "e" }],
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        // @ts-expect-error, not typed
+        const editorView = view.editor.cm as EditorView;
+        const prompt = "This is a prompt"; // TODO get context for prompt
+        this.fetcher
+          .fetch(prompt)
+          .then((suggestion) =>
+            setSuggestionText(editorView, suggestion)
+          );
+      },
+    });
 
-		this.addCommand({
-			id: 'add-current-file-as-context-file',
-			name: 'Add current file as context file',
-			checkCallback: (checking: boolean) => {
-				const activeFile = app.workspace.getActiveFile();
+    this.addCommand({
+      id: "add-current-file-as-context-file",
+      name: "Add current file as context file",
+      checkCallback: (checking: boolean) => {
+        const activeFile = app.workspace.getActiveFile();
 
-				if (activeFile !== null) {
-					if (!checking) {
-						this.contextFiles.add(activeFile.path);
-						console.log(
-							this.getTextForFile(activeFile.path)
-							.then((text: string) => {
-								console.log(text); // Access the resolved text here
-							})
-							.catch((error) => {
-								console.error(error); // Handle any potential errors
-							})
-						);
-					}
-					return true;
-				}
-				return false;
-			},
-		})
+        if (activeFile !== null) {
+          if (!checking) {
+            this.contextFiles.add(activeFile.path);
+            console.log(
+              this.getTextForFile(activeFile.path)
+                .then((text: string) => {
+                  console.log(text); // Access the resolved text here
+                })
+                .catch((error) => {
+                  console.error(error); // Handle any potential errors
+                })
+            );
+          }
+          return true;
+        }
+        return false;
+      },
+    });
 
-		this.registerEditorExtension(ViewPlugin.fromClass(RenderPlugin, renderPluginSpec));
-		this.registerEditorExtension(suggestionField);
-	}
+    this.registerEditorExtension(
+      ViewPlugin.fromClass(RenderPlugin, renderPluginSpec)
+    );
+    this.registerEditorExtension(suggestionField);
+  }
 
-	onunload() {}
+  onunload() { }
 
-	async activateQuillMenuView() {
+  async activateQuillMenuView() {
     this.app.workspace.detachLeavesOfType(VIEW_TYPE_QUILL_MENU);
 
     await this.app.workspace.getRightLeaf(false).setViewState({
@@ -147,11 +183,15 @@ export default class Quill extends Plugin {
     );
   }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async loadSettings() {
+    this.settings = Object.assign(
+      {},
+      DEFAULT_SETTINGS,
+      await this.loadData()
+    );
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
