4540ebc7d955d4758c2596d9596cce0676935b95
Christoph Daxerer <christoph.daxerer@students.fh-hagenberg.at>
2023-07-13 17:15:54 +0200

    Remove template setting

--- settings/Settings.ts
+++ settings/Settings.ts
@@ -8,14 +8,12 @@ export enum ContextBeforeCursorRange {
 }
 
 export interface QuillSettings {
-	mySetting: string;
 	contextBeforeCursor: number;
   contextBeforeCursorRange: ContextBeforeCursorRange;
   contextFileList: ContextFileList; 
 }
 
 export const DEFAULT_SETTINGS: QuillSettings = {
-	mySetting: 'default',
 	contextBeforeCursor: 100,
   contextBeforeCursorRange: ContextBeforeCursorRange.Characters,
   contextFileList: new ContextFileList(),
